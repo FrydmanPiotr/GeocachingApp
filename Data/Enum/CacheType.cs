1c4423f3642485bbd9fbf3313a7ac06c1d68f236
FrydmanPiotr <piotrfrydman99@gmail.com>
2024-09-28 08:16:53 +0200

    Creating models: Address, AppUser, Cache, Club

---
+++ Data/Enum/CacheType.cs
@@ -0,0 +1,14 @@
+﻿namespace GeocachingApp.Data.Enum;
+
+public enum CacheType
+{
+    Traditional,
+    MultiCache,
+    Mystery,
+    LetterboxHybrid,
+    Wherigo,
+    Event,
+    MegaEvent,
+    CITOEvent,
+    Earth
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeocachingApp.Models
+{
+
+    public class AppUser
+    {
+        [Key]
+        public string Id { get; set; }
+        public int? CachesFound { get; set; }
+        public int? CachesCreated { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Cache> Races { get; set; }
+    }
+}
