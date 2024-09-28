41378bf0e167d373d18b6fd6b48866e397a6a159
FrydmanPiotr <piotrfrydman99@gmail.com>
2024-09-28 23:15:16 +0200

    Adding controllers and views for Cache and Club

---
+++ Controllers/CacheController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeocachingApp.Controllers
+{
+    public class CacheController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
