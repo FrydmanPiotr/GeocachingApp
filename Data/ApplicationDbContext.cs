5fcc54456e5581d5a80937bd08ba5eec8a45a0f2
FrydmanPiotr <piotrfrydman99@gmail.com>
2024-09-28 10:07:10 +0200

    Adding DbContext to connect with database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using GeocachingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeocachingApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        
+        }
+        public DbSet<Cache> Caches { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Address { get; set; }
+    }
+
+}
