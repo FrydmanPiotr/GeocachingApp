e6e1342dc5b05b296f2b0bb9e4c60a1d72955919
FrydmanPiotr <piotrfrydman99@gmail.com>
2025-01-04 22:04:43 +0100

    Improved application navigation.

---
+++ wwwroot/js/switch_navbar.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('.nav-link');
+
+    links.forEach(link => {
+        link.addEventListener('click', function () {
+            links.forEach(l => l.classList.remove('active'));
+            link.classList.add('active');
+        });
+    });
+
+    const currentPath = window.location.pathname.split('/').pop();
+    const currentLink = [...links].find(link => link.href.includes(currentPath));
+    if (currentLink) {
+        currentLink.classList.add('active');
+    }
+});
+
